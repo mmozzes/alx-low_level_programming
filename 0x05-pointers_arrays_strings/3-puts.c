5196e15d67e74c3663ca0d47c49c8939f06e9346
mmozzes <mmozzes@gmail.com>
2022-12-19 17:20:40 -0800

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - print a string to the stdout
+ * @str: the string
+ * Return: The length of the string
+ */
+
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a++;
+	}
+}
