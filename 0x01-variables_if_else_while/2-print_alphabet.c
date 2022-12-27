6ad785a6141ae9aba5a636a7b3564f25445f8fc1
mmozzes <mmozzes@gmail.com>
2022-12-27 09:55:57 -0800

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,43 +1,22 @@
 #include <stdio.h>
 
 
-
 /**
- *  * main - print the letters of the alphabet
- *
- *   *
- *
- *    * main: print the letters of the alphabet
- *
- *     *
- *
- *      * Return: Always 0 (Success)
- *
- *
- */
-
-
+* main - print the letters of the alphabet
+* main: print the letters of the alphabet
+* Return: Always 0 (Succes)
+*/
 
 int main(void)
-
 {
-
 	int i = 97;
 
 	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+		putchar(10);
 
-			{
-
-				putchar(i);
-
-						i++;
-
-							}
-
-				putchar(10);
-
-
-
-					return (0);
-
+	return (0);
 }
