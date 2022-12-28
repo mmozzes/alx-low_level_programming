51fc588b39e5f0db818a09c3f22497cccff9b5b7
root <root@mmozzes-PC.localdomain>
2022-12-28 08:22:23 +0300

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *  main - prints all letters af the aplphabet in lowrcase except e and q
+ *  Return: 0
+ */
+
+int main(void)
+{
+	int lowercase = 'a';
+
+	if (lowercase < 'z', lowercase++)
+		putchar(lowercase);
+
+	putchar(10);
+
+	return (0);
+}
