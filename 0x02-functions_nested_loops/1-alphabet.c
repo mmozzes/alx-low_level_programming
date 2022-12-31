8db0410c0df1d7d30ca33e138e4ca785ebbfac1b
mmozzes <mmozzes@gmail.com>
2022-12-31 14:31:42 +0300

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+
+/**
+ * main - prints letters of the alphabet using _putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar(print_alphabet);
+	_putchar('\n');
+
+	return (0);
+}
