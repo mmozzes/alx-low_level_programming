c52247fb78c8483df0ac163e98fd5da737297c64
mmozzes <mmozzes@gmail.com>
2023-01-01 15:09:06 +0300

    Task 3-5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * more_numbers - prints 10 times the number from 0 to 9
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int times = 0;
+		int c = 0;
+
+		while (times <= 0)
+			for (c = 0; c <= 14; c++)
+			{
+				_putchar('0' + c);
+			}
+			_putchar('\n');
+			times++;
+}
